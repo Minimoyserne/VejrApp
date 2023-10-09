f28671ef363139de0f074e2d0a5355bdfa179b3f
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2023-10-09 11:17:21 +0200

    test

--- assets/js/model.js
+++ assets/js/model.js
@@ -1,3 +1,4 @@
+//geo lokation indhentning
 const geoOptions = {
     enableHighAccuracy: true,
     timeout: 5000,
@@ -14,22 +15,81 @@ function success(pos) {
 
 function error(err) {
     console.log(err);
+    // myApp.innerText = `ERROR(${err.code}): ${err.message}`;
 }
 
+function GeoData(myLong, myLat) {
 
-function GeoData(myLat, myLong) {
+    //console.log(myLong, myLat);
     const apiUrl = `https://nominatim.openstreetmap.org/reverse?lat=${myLat}&lon=${myLong}&format=json`;
-    fetch(apiUrl)
-        .then((response) => response.json())
 
-        .then((data) => {
-            console.log('mitData : ', data)
-        });
+    console.log(apiUrl);
+
+    // let mitSted = document.createElement('h2');
+
+    let fetchOptions={
+    
+    Method: 'GET',
+    Body: 'body',
+    Cache: 'default',
+    
+    Headers: {
+        'Accept': 'application.json',
+        'Content-Type': 'application/json'
     }
+    
+    };
 
-<<<<<<< Updated upstream
-=======
-function initApp(){
-   
+    fetch(apiUrl,fetchOptions)
+
+    fetch(apiUrl)
+    .then((response) => response.json())
+    
+    .then((data) => {
+        console.log('my fetched data:', data);
+        
+        // mitSted.innerText = `${data.address.village || data.address.city}`;
+        // console.log(`${data.address.village}`);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        // mitSted.innerText = `my error: ${error}`;
+    });
+    
+    // myApp.appendChild(mitSted);
+    // mitSted.setAttribute("class", "mitsted");
 }
->>>>>>> Stashed changes
+
+    //let grader = "";
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(position => {
+    const { latitude, longitude } = position.coords;
+    
+    // Construct the URL for the weather API
+    //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=YOUR_API_KEY`;
+  
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,precipitation,weathercode,windspeed_10m,winddirection_10m,uv_index&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,rain_sum,windspeed_10m_max,winddirection_10m_dominant&timezone=Europe%2FBerlin`;
+
+    
+    // Fetch the weather data for the user's current location
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+     console.log(data);
+    // dayData = data;
+    // weekData = data; 
+       
+    // makeDayaData(data);
+    // makeWeekData(data);
+
+    //hent grader til vindretning
+    // vindretningNu = data.hourly.winddirection_10m[0];
+    // console.log(vindretningNu);
+
+    // grader = data.grader; // API returnerer grader som bruges til at styre retningen på pilen
+    // myIcon.style.transform = `rotate(${vindretningNu}deg)`; //her rotere pilen alt efter antal grader den får fra API
+    
+
+})
+    .catch(error => console.error(error));
+});
--- assets/js/site.js
+++ assets/js/site.js
@@ -58,100 +58,99 @@ let myIcon = document.getElementById("myIcon");
 myIcon.classList.add("fas", "fa-arrow-down");
 
 //geo lokation indhentning
-const geoOptions = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-};
+// const geoOptions = {
+//     enableHighAccuracy: true,
+//     timeout: 5000,
+//     maximumAge: 0
+// };
 
-navigator.geolocation.getCurrentPosition(success, error, geoOptions);
+// navigator.geolocation.getCurrentPosition(success, error, geoOptions);
 
-function success(pos) {
-    const crd = pos.coords;
+// function success(pos) {
+//     const crd = pos.coords;
 
-    GeoData(crd.longitude, crd.latitude);
-}
+//     GeoData(crd.longitude, crd.latitude);
+// }
 
-function error(err) {
-    myApp.innerText = `ERROR(${err.code}): ${err.message}`;
-}
+// function error(err) {
+//     myApp.innerText = `ERROR(${err.code}): ${err.message}`;
+// }
 
-function GeoData(myLong, myLat) {
+// function GeoData(myLong, myLat) {
 
-    //console.log(myLong, myLat);
-    const apiUrl = `https://nominatim.openstreetmap.org/reverse?lat=${myLat}&lon=${myLong}&format=json`;
+//     //console.log(myLong, myLat);
+//     const apiUrl = `https://nominatim.openstreetmap.org/reverse?lat=${myLat}&lon=${myLong}&format=json`;
 
-    console.log(apiUrl);
+//     console.log(apiUrl);
 
-    let mitSted = document.createElement('h2');
+//     // let mitSted = document.createElement('h2');
 
-    let fetchOptions={
+//     let fetchOptions={
     
-    Method: 'GET',
-    Body: 'body',
-    Cache: 'default',
+//     Method: 'GET',
+//     Body: 'body',
+//     Cache: 'default',
     
-    Headers: {
-        'Accept': 'application.json',
-        'Content-Type': 'application/json'
-    }
+//     Headers: {
+//         'Accept': 'application.json',
+//         'Content-Type': 'application/json'
+//     }
     
-    };
+//     };
 
-    fetch(apiUrl,fetchOptions)
+//     fetch(apiUrl,fetchOptions)
 
-    fetch(apiUrl)
-    .then((response) => response.json())
+//     fetch(apiUrl)
+//     .then((response) => response.json())
     
-    .then((data) => {
-        //console.log('my fetched data:', data);
+//     .then((data) => {
+//         console.log('my fetched data:', data);
         
-        mitSted.innerText = `${data.address.village || data.address.city}`;
-        console.log(`${data.address.village}`);
-        //Er dette rigtigt? Det virkede med address.city på skolen, men hjemme sulle jeg bruge address.village. Dokumentationene siger address.city ?
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-        mitSted.innerText = `my error: ${error}`;
-    });
+//         // mitSted.innerText = `${data.address.village || data.address.city}`;
+//         // console.log(`${data.address.village}`);
+//     })
+//     .catch((error) => {
+//         console.error('Error:', error);
+//         // mitSted.innerText = `my error: ${error}`;
+//     });
     
-    myApp.appendChild(mitSted);
-    mitSted.setAttribute("class", "mitsted");
-}
+//     // myApp.appendChild(mitSted);
+//     // mitSted.setAttribute("class", "mitsted");
+// }
 
-    let grader = "";
-    // Get the user's current location
-    navigator.geolocation.getCurrentPosition(position => {
-    const { latitude, longitude } = position.coords;
+//     //let grader = "";
+//     // Get the user's current location
+//     navigator.geolocation.getCurrentPosition(position => {
+//     const { latitude, longitude } = position.coords;
     
-    // Construct the URL for the weather API
-    //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=YOUR_API_KEY`;
+//     // Construct the URL for the weather API
+//     //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=YOUR_API_KEY`;
   
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,precipitation,weathercode,windspeed_10m,winddirection_10m,uv_index&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,rain_sum,windspeed_10m_max,winddirection_10m_dominant&timezone=Europe%2FBerlin`;
+//     const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,precipitation,weathercode,windspeed_10m,winddirection_10m,uv_index&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,rain_sum,windspeed_10m_max,winddirection_10m_dominant&timezone=Europe%2FBerlin`;
 
     
-    // Fetch the weather data for the user's current location
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-    console.table(data);
-    dayData = data;
-    weekData = data; 
+//     // Fetch the weather data for the user's current location
+//     fetch(url)
+//       .then((response) => response.json())
+//       .then((data) => {
+//     console.table(data);
+//     dayData = data;
+//     weekData = data; 
        
-    makeDayaData(data);
-    makeWeekData(data);
+//     makeDayaData(data);
+//     makeWeekData(data);
 
-    //hent grader til vindretning
-    vindretningNu = data.hourly.winddirection_10m[0];
-    console.log(vindretningNu);
+//     //hent grader til vindretning
+//     // vindretningNu = data.hourly.winddirection_10m[0];
+//     console.log(vindretningNu);
 
-    grader = data.grader; // API returnerer grader som bruges til at styre retningen på pilen
-    myIcon.style.transform = `rotate(${vindretningNu}deg)`; //her rotere pilen alt efter antal grader den får fra API
+//     // grader = data.grader; // API returnerer grader som bruges til at styre retningen på pilen
+//     // myIcon.style.transform = `rotate(${vindretningNu}deg)`; //her rotere pilen alt efter antal grader den får fra API
     
 
-})
-    .catch(error => console.error(error));
-});
+// })
+//     .catch(error => console.error(error));
+// });
 
 
 
@@ -426,6 +425,7 @@ function makeWeekData(data) {
 if (!(/iPhone|iPad|iPod|Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
     // DOM-kode til PC
     console.log("Dette er en pc-enhed");
+    
     // Start på view kode til PC
   //  let mitSted = document.createElement("h2");
   //  mitSted.setAttribute("class", "pc-element");
