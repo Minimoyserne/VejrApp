2c487364411eccdf9b314c9862051faa2d768d49
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2023-10-24 09:06:35 +0200

    SW installeret

--- assets/js/weatherdata.controller.js
+++ assets/js/weatherdata.controller.js
@@ -172,3 +172,13 @@ export function DataConversion(data, myPlace){
 
 }
 
+if('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('./serviceWorker.js')
+	.then(reg => console.log('service worker registered', reg))
+	.catch(err => console.error('service worker not registered', err)) 
+}
+
+// Fetch event
+self.addEventListener('fetch', event => {
+	console.log('Fetch event', event)
+})
